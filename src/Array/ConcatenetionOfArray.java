7b1dc083543244bdf296aa29a20cbfb2f4399bf8
Mo11y <85448700+kir4che@users.noreply.github.com>
2022-02-10 00:22:53 +0800

    Add files via upload

---
+++ src/Array/ConcatenetionOfArray.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ConcatenetionOfArray {
+    public class BuildArrfromPerm {
+        public static int[] Solution(int[] nums) {
+            int[] ans = new int[nums.length*2];
+            for (int i = 0; i < nums.length; i++) {
+                ans[i] = nums[i];
+                ans[i+nums.length] = nums[i];
+            }
+            return ans;
+        }
+        public static void main(String[] args) {
+            int[] data = {1,2,1};
+            System.out.println(Arrays.toString(Solution(data)));
+        }
+    }
+}
