f41382ad1b3c7214afe1941b74ad0e32ef1f8402
Mo11y <85448700+kir4che@users.noreply.github.com>
2022-02-10 16:12:57 +0800

    Update RunningSumOf1dArray.java

--- src/Array/RunningSumOf1dArray.java
+++ src/Array/RunningSumOf1dArray.java
@@ -1,4 +1,19 @@
 package Array;
 
+import java.util.Arrays;
+
 public class RunningSumOf1dArray {
+    public static int[] Solution(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            ans[i] = nums[i] + ans[i-1];
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println(Arrays.toString((Solution(arr))));
+    }
+
 }
