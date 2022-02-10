f0f82e38ce69b6d3168bca85454090c2f213ff7e
Mo11y <85448700+kir4che@users.noreply.github.com>
2022-02-10 15:44:23 +0800

    Update ConcatenetionOfArray.java

--- src/Array/ConcatenetionOfArray.java
+++ src/Array/ConcatenetionOfArray.java
@@ -3,18 +3,16 @@ package Array;
 import java.util.Arrays;
 
 public class ConcatenetionOfArray {
-    public class BuildArrfromPerm {
-        public static int[] Solution(int[] nums) {
-            int[] ans = new int[nums.length*2];
-            for (int i = 0; i < nums.length; i++) {
-                ans[i] = nums[i];
-                ans[i+nums.length] = nums[i];
-            }
-            return ans;
+    public static int[] Solution(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
         }
-        public static void main(String[] args) {
-            int[] data = {1,2,1};
-            System.out.println(Arrays.toString(Solution(data)));
-        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] data = {1,2,1};
+        System.out.println(Arrays.toString(Solution(data)));
     }
 }
