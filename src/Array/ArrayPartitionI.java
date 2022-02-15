65280ccfecd4c922ea9116b731d5e8a0ae3e9eea
Mo11y <85448700+kir4che@users.noreply.github.com>
2022-02-15 10:55:50 +0800

    Add files via upload

--- src/Array/ArrayPartitionI.java
+++ src/Array/ArrayPartitionI.java
@@ -1,4 +1,18 @@
 package Array;
 
+import java.util.Arrays;
+
 public class ArrayPartitionI {
+    public static int Solution(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++)
+            sum += nums[i++];
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,3,2};
+        System.out.println(Solution(arr));
+    }
 }
--- src/Array/BuildArrayFromPermutation.java
+++ src/Array/BuildArrayFromPermutation.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BuildArrayFromPermutation {
     public static int[] Solution(int[] nums) {
         int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++)
+        for(int i = 0; i < nums.length; i++)
             ans[i] = nums[nums[i]];
         return ans;
     }
